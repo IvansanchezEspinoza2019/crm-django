4c0e4195e20d186bb0e45a8feb9eb235c235d421
Iván Sánchez <edgar.sanchez1492@alumnos.udg.mx>
2022-04-19 21:23:24 -0500

    leads ist view created

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse        
+from .models import Lead
 
 # Create your views here.
+def leads_list(request):
+    #return HttpResponse("Hello world")
+    leads = Lead.objects.all()
+    context =  {
+       "leads": leads
+    }
+    return render(request, "leads/leads_list.html",context)
