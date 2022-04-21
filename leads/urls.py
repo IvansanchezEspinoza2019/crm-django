d3b7289036f9b7a835c29bfa8fefb5d96491fab0
Iván Sánchez <edgar.sanchez1492@alumnos.udg.mx>
2022-04-20 21:16:59 -0500

    Workin with django forms

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import leads_list
+from .views import leads_list, load_detail, lead_create
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', leads_list)
+    path('', leads_list),
+    path('create', lead_create),
+    path('<int:pk>/', load_detail)
 ]
