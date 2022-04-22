e2118711811a383acca52277a155a91d281526dc
Iván Sánchez <edgar.sanchez1492@alumnos.udg.mx>
2022-04-21 20:42:51 -0500

    Leads CRUD

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import leads_list, load_detail, lead_create
+from .views import leads_list, load_detail, lead_create, lead_updtae, lead_delete
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', leads_list),
-    path('create', lead_create),
-    path('<int:pk>/', load_detail)
+    path('', leads_list, name='lead-list'),
+    path('create/', lead_create,  name='lead-create'),
+    path('<int:pk>/', load_detail,  name='lead-detail'),
+    path('<int:pk>/update/', lead_updtae, name='lead-update'),
+    path('<int:pk>/delete/', lead_delete, name='lead-delete')
 ]
